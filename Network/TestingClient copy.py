b699736dbeac2eabc087420f3cf08ec80fd26202
Oscar Clausen <occlausenoscar12@gmail.com>
2025-07-15 10:49:44 +0100

    Multiple connections works

---
+++ Network/TestingClient copy.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+
+def receive_messages(sock):
+    buffer = ""
+    while True:
+        char = sock.recv(1).decode()
+        if not char:
+            break
+        if char == ";":
+            print(f"Server says: {buffer}")
+            buffer = ""
+        else:
+            buffer += char
+
+s = socket.socket()
+s.connect(('localhost', 12345))
+
+# Start receiver in background
+threading.Thread(target=receive_messages, args=(s,), daemon=True).start()
+
+# Input loop to send data
+print("You can now type messages to send to the server:")
+try:
+    while True:
+        msg = input()
+        s.sendall((msg + ";").encode())
+except KeyboardInterrupt:
+    print("\nClient stopped.")
